adcb544391bd1d96d44dd476cdb8f524177f5a26
vGuxd <156451767+vGuxd@users.noreply.github.com>
2024-02-09 13:55:59 +0100

    Add files via upload

--- mailer/form_setting.php
+++ mailer/form_setting.php
@@ -6,3 +6,39 @@
 	$fromName = 'Your Company Name'; // Your company name
 	$charset = 'UTF-8';
 ?>
+
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Get form data
+    $name = $_POST["name"];
+    $tel = $_POST["tel"];
+    $email = $_POST["email"];
+    $subject = $_POST["subject"];
+    $message = $_POST["message"];
+
+    // Email address to receive form submissions
+    $to = "vguxd14@gmail.com";
+
+    // Email subject
+    $email_subject = "New Contact Form Submission: $subject";
+
+    // Email headers
+    $headers = "From: $email\r\n";
+    $headers .= "Reply-To: $email\r\n";
+    $headers .= "X-Mailer: PHP/" . phpversion();
+
+    // Compose email message
+    $email_message = "Name: $name\n";
+    $email_message .= "Phone: $tel\n";
+    $email_message .= "Email: $email\n";
+    $email_message .= "Subject: $subject\n\n";
+    $email_message .= "Message:\n$message";
+
+    // Send email
+    mail($to, $email_subject, $email_message, $headers);
+
+    // You can add additional actions here, such as redirecting to a thank you page
+    // header("Location: thank-you.html");
+    // exit();
+}
+?>
