9b9fcb3b0afe71f519cfac1277ce2879b20180c7
vGuxd <156451767+vGuxd@users.noreply.github.com>
2024-02-09 13:15:54 +0100

    Add files via upload

--- mailer/form_setting.php
+++ mailer/form_setting.php
@@ -1,7 +1,7 @@
 <?php
 	/*Form settings*/
 	$subj = "New message from the site 'You Site Name'"; //letter subject
-	$to = 'beshleygame@mail.ru'; // Enter Your E-mail
+	$to = 'vguxd14@gmail.com'; // Enter Your E-mail
 	$from = 'admin@you-site-name.com'; // Admin e-mail
 	$fromName = 'Your Company Name'; // Your company name
 	$charset = 'UTF-8';
